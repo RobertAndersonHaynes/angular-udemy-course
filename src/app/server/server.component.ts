f4e7ba4a4c9b70eb506ee71c309c79086ab8f21d
Haynes <rhayne071@cable.comcast.com>
2020-06-24 17:50:55 -0400

    completed basic assignment 2

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,9 +1,24 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html'
 })
-export class ServerComponent {
 
+export class ServerComponent implements OnInit {
+  userName = "";
+  //allowClick = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  // setClick() {
+  //   if (this.userName != "") {
+  //     this.allowClick = true
+  //   }
+  // }
+  onButtonClick() {
+    this.userName = ""
+  }
 }
