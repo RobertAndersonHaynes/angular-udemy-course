6b1519e6cafb20346c82226f75f30ac1980a33b8
Haynes <rhayne071@cable.comcast.com>
2020-07-06 16:39:27 -0400

    Assignemnt 3 directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: [`
+    .clicks {
+      color: white;
+    }
+  `]
 })
 export class AppComponent {
+  detailsClicked = false;
+  x = 1
+  clickLog = [];
 
+  onDetailsButtonClick() {
+    this.detailsClicked = !this.detailsClicked
+    this.clickLog.push(this.x);
+    this.x++
+  }
+
+  // getColor() {
+  //   return this.clickLog.length < 5 ? 'white' : 'blue'
+  // }
 }
